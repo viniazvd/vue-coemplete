8b3fb51429d900d3e88dee2049202e172ca10a8f
Vinicius Azevedo <viniazvd@gmail.com>
2020-01-14 16:05:09 -0300

    :zap: :art: improve examples

--- examples/Index.vue
+++ examples/Index.vue
@@ -1,27 +1,18 @@
 <template>
   <div id="app">
-    <vue-coemplete placeholder="Pesquisar" :options="options" :display-by="['area']">
-      <input class="input" slot-scope="{ onSearch }" @input="event => onSearch(event.target.value)" />
-    </vue-coemplete>
-
-    <vue-coemplete placeholder="Pesquisar" :options="options" :display-by="['area']" />
+    <with-slots />
+    <without-slots />
   </div>
 </template>
 
 <script>
-import data from './data'
-import { VueCoemplete } from '../dist/bundle'
+import WithSlots from './WithSlots.vue'
+import WithoutSlots from './WithoutSlots.vue'
 
 export default {
   name: 'example',
 
-  components: { VueCoemplete },
-
-  computed: {
-    options () {
-      return data
-    }
-  }
+  components: { WithSlots, WithoutSlots }
 }
 </script>
 
@@ -29,13 +20,5 @@ export default {
 #app {
   display: flex;
   justify-content: space-evenly;
-}
-
-.input {
-  width: 100%;
-  border-radius: 20px;
-  border: 0;
-  outline: none;
-  padding-left: 20px;
 }
 </style>
