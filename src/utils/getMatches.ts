628c21dc49ea9079ac79558011c02cdda1a88c2a
Vinicius Azevedo <vinicius.azevedo@convenia.com.br>
2020-02-14 14:03:05 -0300

    :hankey: remove log

--- src/utils/getMatches.ts
+++ src/utils/getMatches.ts
@@ -3,7 +3,6 @@ import normalizeDiacritics from './normalizeDiacritics'
 function getMatches (word = '', words = '') {
   const normalizedWord = normalizeDiacritics(word)
   const normalizedWords = normalizeDiacritics(words)
-  console.log({ normalizedWord, normalizedWords })
 
   return normalizedWords.toLowerCase().split(normalizedWord.toLowerCase())
 }
